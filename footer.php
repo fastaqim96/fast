62cae2fb877710beae25f315c39b0640a79e740a
fastaqim96 <86286565+fastaqim96@users.noreply.github.com>
2022-11-09 11:58:16 +0800

    Update footer.php
    Update the copyright year.

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="container-fluid clearfix">
 	<span class="text-muted d-block text-center text-sm-left d-sm-inline-block">
-		Copyright © 2017-2020 <a href="#" target="_blank">Fastaqim Resources</a>. All rights reserved.
+		Copyright © 2017-2022 <a href="#" target="_blank">Fastaqim Resources</a>. All rights reserved.
 	</span>
 	<!-- <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="mdi mdi-heart text-danger"></i>
 	</span> -->
